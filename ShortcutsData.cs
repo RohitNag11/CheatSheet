0ee18f4397d6e701995cf88a91382fcbd6e76727
Baron Khan <baronkhan@microsoft.com>
2022-09-21 11:35:50 +0100

    Add JSON parsing

---
+++ ShortcutsData.cs
@@ -0,0 +1,75 @@
+﻿class ShortcutsData
+{
+    public static string Shortcuts = @"
+{
+  ""Apps"": [
+    {
+      ""Name"": ""notepad.exe"",
+      ""FriendlyName"": ""Notepad"",
+      ""Shortcuts"": [
+        {
+          ""Modifier"": ""Ctrl"",
+          ""Key"": ""C"",
+          ""Description"": ""Copy text"",
+          ""Extended"": ""Copy text to clipboard""
+        },
+        {
+          ""Modifier"": ""Ctrl"",
+          ""Key"": ""V"",
+          ""Description"": ""Paste text"",
+          ""Extended"": ""Paste text from clipboard""
+        },
+        {
+          ""Modifier"": ""Ctrl"",
+          ""Key"": ""X"",
+          ""Description"": ""Cut text"",
+          ""Extended"": ""Cut text to clipboard""
+        },
+        {
+          ""Modifier"": ""Ctrl"",
+          ""Key"": ""Z"",
+          ""Description"": ""Undo"",
+          ""Extended"": ""Undo last change""
+        }
+      ]
+    },
+    {
+      ""Name"": ""HxOutlook.exe"",
+      ""FriendlyName"": ""Mail"",
+      ""Shortcuts"": [
+        {
+          ""Modifier"": ""Ctrl"",
+          ""Key"": ""N"",
+          ""Description"": ""New mail"",
+          ""Extended"": ""Begin a new e-mail""
+        },
+        {
+          ""Modifier"": ""Ctrl"",
+          ""Key"": ""R"",
+          ""Description"": ""Reply"",
+          ""Extended"": ""Reply to the selected e-mail""
+        }
+      ]
+    },
+    {
+      ""Name"": ""devenv.exe"",
+      ""FriendlyName"": ""Visual Studio"",
+      ""Shortcuts"": [
+        {
+          ""Modifier"": ""Ctrl"",
+          ""Key"": ""O"",
+          ""Description"": ""Open file"",
+          ""Extended"": ""Open a file""
+        },
+        {
+          ""Modifier"": ""Ctrl"",
+          ""Key"": ""P"",
+          ""Description"": ""Go to file"",
+          ""Extended"": ""Go to a file""
+        }
+      ]
+    }
+  ]
+}
+";
+}
