36e440e5d1cd7b049c69cf25b8341b4d9eb79b95
Baron Khan <baronkhan@microsoft.com>
2022-09-21 15:39:31 +0100

    Update JSON schema for command chaining

--- Shortcuts.cs
+++ Shortcuts.cs
@@ -1,12 +1,6 @@
-﻿// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+﻿// ShortcutsJsonRoot myDeserializedClass = JsonConvert.DeserializeObject<ShortcutsJsonRoot>(myJsonResponse);
 using Newtonsoft.Json;
 using System.Collections.Generic;
-
-public class ShortcutsJsonRoot
-{
-    [JsonProperty("Apps")]
-    public List<App> Apps { get; set; }
-}
 
 public class App
 {
@@ -16,22 +10,34 @@ public class App
     [JsonProperty("FriendlyName")]
     public string FriendlyName { get; set; }
 
-    [JsonProperty("Shortcuts")]
-    public List<Shortcut> Shortcuts { get; set; }
+    [JsonProperty("ShortcutGroups")]
+    public List<ShortcutGroup> ShortcutGroups { get; set; }
+}
+
+public class ShortcutsJsonRoot
+{
+    [JsonProperty("Apps")]
+    public List<App> Apps { get; set; }
 }
 
 public class Shortcut
 {
-    [JsonProperty("Modifier")]
-    public string Modifier { get; set; }
-
-    [JsonProperty("Key")]
-    public string Key { get; set; }
+    [JsonProperty("Keys")]
+    public List<List<string>> Keys { get; set; }
 
     [JsonProperty("Description")]
     public string Description { get; set; }
 
     [JsonProperty("Extended")]
     public string Extended { get; set; }
+}
+
+public class ShortcutGroup
+{
+    [JsonProperty("Name")]
+    public string Name { get; set; }
+
+    [JsonProperty("Shortcuts")]
+    public List<Shortcut> Shortcuts { get; set; }
 }
 
